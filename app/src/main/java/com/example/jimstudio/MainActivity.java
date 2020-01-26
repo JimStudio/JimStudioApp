1c029f9cb881c4cd12799b6afbe0aed0b4de1113
JimStudio <chuixiaojian@hotmail.com>
2020-01-27 04:29:24 +0800

    new layout function

--- app/src/main/java/com/example/jimstudio/MainActivity.java
+++ app/src/main/java/com/example/jimstudio/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String passwprd = 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
