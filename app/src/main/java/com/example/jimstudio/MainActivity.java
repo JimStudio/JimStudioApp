ecb30c203d30684bb710a68f0bb352f1f1224bf9
JimStudio <chuixiaojian@hotmail.com>
2020-01-27 04:33:43 +0800

    new layout function

--- app/src/main/java/com/example/jimstudio/MainActivity.java
+++ app/src/main/java/com/example/jimstudio/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.jimstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    String passwprd = 
+    EditText user = (EditText)findViewById(R.id.user_login_edit);
+    EditText password = (EditText)findViewById(R.id.password_edit);
+    Button Login = (Button)findViewById(R.id.login_bt);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
