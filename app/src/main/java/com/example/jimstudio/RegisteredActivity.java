51bed38b6d6e81671523539b6d20b1c764187330
JimStudio <chuixiaojian@hotmail.com>
2020-04-08 13:52:38 +0800

    new fun

--- app/src/main/java/com/example/jimstudio/RegisteredActivity.java
+++ app/src/main/java/com/example/jimstudio/RegisteredActivity.java
@@ -3,12 +3,24 @@ package com.example.jimstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 
 public class RegisteredActivity extends AppCompatActivity {
+
+    EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registered);
+        iten();
+        
+    }
+
+    public void iten(){
+        username = (EditText)findViewById(R.id.re_username);
     }
 }
