ea21a31061934d2e91f61bb8befd18004ab17fb7
JimStudio <chuixiaojian@hotmail.com>
2020-06-25 13:04:50 +0800

    new fun

--- app/src/main/java/com/example/jimstudio/RegisteredActivity.java
+++ app/src/main/java/com/example/jimstudio/RegisteredActivity.java
@@ -3,6 +3,7 @@ package com.example.jimstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.TestLooperManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -11,9 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class RegisteredActivity extends AppCompatActivity {
 
     EditText username , password , confrompassword , Email;
+    TextInputLayout username_text , comfrom_password_text , email_test;
     Button bt_register;
 
     @Override
@@ -21,30 +25,84 @@ public class RegisteredActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registered);
         iten();
-        password.setHint("Password");
-        confrompassword.setHint("Conform Password");
+        hint_test();
+        username.addTextChangedListener(new RegisterdTextWatcher(username));
 
 
-        bt_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String pas = password.getText().toString();
-                final String compass = confrompassword.getText().toString();
-                if (pas.equals(compass)){
-                    Toast.makeText(getApplicationContext() , "Pass" , Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getApplicationContext(), "No Pass" , Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+//        bt_register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                final String pas = password.getText().toString();
+//                final String compass = confrompassword.getText().toString();
+//                if (pas.equals(compass)){
+//                    Toast.makeText(getApplicationContext() , "Pass" ,
+//                            Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Toast.makeText(getApplicationContext(), "No Pass" , Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
 
     }
 
+    public void hint_test(){
+        username.setHint(R.string.user);
+        password.setHint(R.string.password);
+        confrompassword.setHint(R.string.comfrom_password);
+        Email.setHint(R.string.email);
+    }
+
     public void iten(){
-//        username = (EditText)findViewById(R.id.re_username);
+       username = (EditText)findViewById(R.id.re_user);
         password = (EditText) findViewById(R.id.re_password);
         confrompassword = (EditText) findViewById(R.id.re_confirmPassword);
         Email = (EditText) findViewById(R.id.re_email);
         bt_register = (Button) findViewById(R.id.re_bt);
+        username_text = (TextInputLayout) findViewById(R.id.re_text_user);
+        comfrom_password_text = (TextInputLayout) findViewById(R.id.re_test_password_comform);
+        email_test = (TextInputLayout) findViewById(R.id.re_test_email);
+    }
+
+    public boolean validateName(){
+
+        String username_validate = username.getText().toString().trim();
+        if (username_validate.length() < 8 || username_validate.length() > 17){
+            username.setError(getString(R.string.long_error));
+
+            return false;
+        }else{
+            username_text.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private class RegisterdTextWatcher implements TextWatcher {
+
+        private View view;
+        private RegisterdTextWatcher(View view){
+            this.view = view;
+        }
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+            switch (view.getId()){
+
+                case R.id.re_user:
+                    Log.d("Hallo" , "ok");
+                    validateName();
+                    break;
+            }
+        }
     }
 }
+
+
